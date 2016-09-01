fa338ad1fce73fa1cc3353cfcc9ed7f3bac87e6d
Simon Garcia <garcias@kenyon.edu>
2016-09-01 17:22:06 +0000

    run from repository

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo VNC client running at https://$C9_HOSTNAME/vnc.html
+echo Open a new terminal and run the desired application
 echo
 
-cd /home/ubuntu/workspace
 supervisord -c supervisord.conf
