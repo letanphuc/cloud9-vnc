3bfc66e419197e27d0ba7f7574712b53e1def2ee
Simon Garcia <garcias@kenyon.edu>
2016-09-03 02:51:23 +0000

    check for host ip and set VNC_IP

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -v C9_HOSTNAME ]
+then
+    VNC_IP=$C9_HOSTNAME
+else
+    VNC_IP=$(cat /etc/network/interfaces | grep "address" | sed 's/^[ \t]*//' | sed 's/address //')
+fi
+
 echo VNC client running at https://$C9_HOSTNAME/vnc.html
 echo Open a new terminal and run the desired application
 echo
