ea2a24a0375a2ebb5c396dbd489f3abff6a590b4
Simon Garcia <garcias@kenyon.edu>
2016-09-03 02:52:02 +0000

    update run message

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ else
     VNC_IP=$(cat /etc/network/interfaces | grep "address" | sed 's/^[ \t]*//' | sed 's/address //')
 fi
 
-echo VNC client running at https://$C9_HOSTNAME/vnc.html
-echo Open a new terminal and run the desired application
+echo VNC client running at https://$VNC_IP/vnc.html
+echo Open a new shell and run the desired application
 echo
 
 supervisord -c supervisord.conf
